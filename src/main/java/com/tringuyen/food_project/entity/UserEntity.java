8fe2487cf2ad5f974f6610cca37c3a40cfca5a6c
Tri Nguyen <ttth0333@gmail.com>
2022-11-21 21:58:00 +0700

    feature/login

---
+++ src/main/java/com/tringuyen/food_project/entity/UserEntity.java
@@ -0,0 +1,117 @@
+package com.tringuyen.food_project.entity;
+
+import javax.persistence.*;
+
+@Entity(name = "user")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "fullname")
+    private String fullname;
+
+    @Column(name = "token")
+    private String token;
+
+    @Column(name = "type_token")
+    private String typeToken;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "verify_code")
+    private String verifyCode;
+
+    @Column(name = "verify_code_expired")
+    private String verifyCodeExpired;
+
+    @Column(name = "is_active")
+    private boolean isActive;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getTypeToken() {
+        return typeToken;
+    }
+
+    public void setTypeToken(String typeToken) {
+        this.typeToken = typeToken;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getVerifyCode() {
+        return verifyCode;
+    }
+
+    public void setVerifyCode(String verifyCode) {
+        this.verifyCode = verifyCode;
+    }
+
+    public String getVerifyCodeExpired() {
+        return verifyCodeExpired;
+    }
+
+    public void setVerifyCodeExpired(String verifyCodeExpired) {
+        this.verifyCodeExpired = verifyCodeExpired;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+}
---
+++ src/main/java/com/tringuyen/food_project/service/LoginService.java
@@ -0,0 +1,9 @@
+package com.tringuyen.food_project.service;
+
+import com.tringuyen.food_project.entity.UserEntity;
+
+import java.util.List;
+
+public interface LoginService {
+    boolean checkLogin(String email, String password);
+}
